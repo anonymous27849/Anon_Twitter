74142c5998dcd79057386871138fef5a642c39cd
sabughnaim <sara15@u.northwestern.edu>
2015-11-21 20:27:46 -0600

    updated report

--- twitterReport.py
+++ twitterReport.py
@@ -14,11 +14,14 @@ from datetime import datetime
 import splinter
 from splinter.request_handler.status_code import HttpResponseError
 import getpass
+from selenium import webdriver
+
+f = webdriver.Firefox()
 
 def main(argv):
     d = datetime.now()
     date = str(d.year) + '' + str(d.month) + '' + str(d.day) + '' + str(d.hour) + '' + str(d.minute) + '' + str(d.second)
-    username = None
+    username = "wildhacks"
     txt = None
     try:
         opts, args = getopt.getopt(argv,"hi:u:",["file=","user=","help"])
@@ -49,87 +52,88 @@ def main(argv):
 # comment this line if you want to use privoxy + tor:
    # with Browser() as browser:
 #uncomment this section if you want to use privoxy + tor:
-proxyIP = '127.0.0.1'
-proxyPort = 8118
+    proxyIP = '127.0.0.1'
+    proxyPort = 8118
 
-proxy_settings = {'network.proxy.type': 1,
-        'network.proxy.http': proxyIP,
-       'network.proxy.http_port': proxyPort,
-       'network.proxy.ssl': proxyIP,
-       'network.proxy.ssl_port':proxyPort,
-       'network.proxy.socks': proxyIP,
-       'network.proxy.socks_port':proxyPort,
-       'network.proxy.ftp': proxyIP,
-       'network.proxy.ftp_port':proxyPort
-       }
+    proxy_settings = {'network.proxy.type': 1,
+            'network.proxy.http': proxyIP,
+           'network.proxy.http_port': proxyPort,
+           'network.proxy.ssl': proxyIP,
+           'network.proxy.ssl_port':proxyPort,
+           'network.proxy.socks': proxyIP,
+           'network.proxy.socks_port':proxyPort,
+           'network.proxy.ftp': proxyIP,
+           'network.proxy.ftp_port':proxyPort
+           }
 
-with Browser('firefox',profile_preferences=proxy_settings) as browser:
-    browser.visit("https://twitter.com/")
-    browser.execute_script("$('#front-container #signin-email').val('%s');"  % (username))
-    browser.execute_script("$('#front-container #signin-password').val('%s');" % (password))
-    browser.find_by_css("button[type='submit'].submit.btn.primary-btn").click()
+    with Browser("firefox",profile_preferences=proxy_settings) as browser:
+        browser.visit("https://twitter.com/")
+        print username 
+        browser.execute_script("$('#front-container #signin-email').val('%s');"  % (username))
+        browser.execute_script("$('#front-container #signin-password').val('%s');" % (password))
+        browser.find_by_css("button[type='submit'].submit.btn.primary-btn").click()
 
-    if "https://twitter.com/login/error" in browser.url:
-        print "The email and password you entered did not match our records."
-        sys.exit()
+        if "https://twitter.com/login/error" in browser.url:
+            print "The email and password you entered did not match our records."
+            sys.exit()
 
-    try:
-        file = open(txt, 'r')
-    except:
-        print "Unable to open file"
-        sys.exit()
-
-    for line in file:
         try:
-            url = re.match(r"(?:https:\/\/)?(?:http:\/\/)?(?:www\.)?twitter\.com/(#!/)?@?([^/\s]*)",line.strip())
-            url = url.group()
-            browser.visit(url)
-            if not browser.is_element_present_by_css('.route-account_suspended'):
-                browser.find_by_css('.user-dropdown').click()
-                browser.find_by_css('li.report-text button[type="button"]').click()
-                with browser.get_iframe('new-report-flow-frame') as iframe:
-                    iframe.find_by_css("input[type='radio'][value='abuse']").check()
-                browser.find_by_css('.new-report-flow-next-button').click()
-                with browser.get_iframe('new-report-flow-frame') as iframe:
-                    iframe.find_by_css("input[type='radio'][value='harassment']").check()
-                browser.find_by_css('.new-report-flow-next-button').click()
-                with browser.get_iframe('new-report-flow-frame') as iframe:
-                    iframe.find_by_css("input[type='radio'][value='Someone_else']").check()
-                browser.find_by_css('.new-report-flow-next-button').click()
-                with browser.get_iframe('new-report-flow-frame') as iframe:
-                    iframe.find_by_css("input[type='radio'][value='violence']").check()
-                browser.find_by_css('.new-report-flow-next-button').click()
-                followers = browser.find_by_css('a[data-nav="followers"] .ProfileNav-value').value;
-                msg = url.strip()+' - ' + followers + ' Followers'
-                with open("log_reported_"+date+".txt", "a") as log:
-                    log.write(msg+"\n")
-            elif browser.is_element_present_by_css('.route-account_suspended'):
-                msg =  line.strip()+' - Suspended'
-                with open("log_suspended.txt", "a") as log:
-                    log.write(msg+"\n")
-            else:
-                msg = line.strip()+' - Unknown'
-                with open("log_unknown.txt", "a") as log:
-                    log.write(msg+"\n")
-
-            print msg
-
-        except KeyboardInterrupt:
-            print 'Quit by keyboard interrupt sequence!'
-            break
-        except HttpResponseError:
-            msg = line.strip()+' - HttpResponseError'
-            print msg
-            with open("log_Error.txt", "a") as log:
-                log.write(msg+"\n")
+            file = open(txt, 'r')
         except:
-            if line:
-                msg = url.strip()+' - CatchAllError'
+            print "Unable to open file"
+            sys.exit()
+
+        for line in file:
+            try:
+                url = re.match(r"(?:https:\/\/)?(?:http:\/\/)?(?:www\.)?twitter\.com/(#!/)?@?([^/\s]*)",line.strip())
+                url = url.group()
+                browser.visit(url)
+                if not browser.is_element_present_by_css('.route-account_suspended'):
+                    browser.find_by_css('.user-dropdown').click()
+                    browser.find_by_css('li.report-text button[type="button"]').click()
+                    with browser.get_iframe('new-report-flow-frame') as iframe:
+                        iframe.find_by_css("input[type='radio'][value='abuse']").check()
+                    browser.find_by_css('.new-report-flow-next-button').click()
+                    with browser.get_iframe('new-report-flow-frame') as iframe:
+                        iframe.find_by_css("input[type='radio'][value='harassment']").check()
+                    browser.find_by_css('.new-report-flow-next-button').click()
+                    with browser.get_iframe('new-report-flow-frame') as iframe:
+                        iframe.find_by_css("input[type='radio'][value='Someone_else']").check()
+                    browser.find_by_css('.new-report-flow-next-button').click()
+                    with browser.get_iframe('new-report-flow-frame') as iframe:
+                        iframe.find_by_css("input[type='radio'][value='violence']").check()
+                    browser.find_by_css('.new-report-flow-next-button').click()
+                    followers = browser.find_by_css('a[data-nav="followers"] .ProfileNav-value').value;
+                    msg = url.strip()+' - ' + followers + ' Followers'
+                    with open("log_reported_"+date+".txt", "a") as log:
+                        log.write(msg+"\n")
+                elif browser.is_element_present_by_css('.route-account_suspended'):
+                    msg =  line.strip()+' - Suspended'
+                    with open("log_suspended.txt", "a") as log:
+                        log.write(msg+"\n")
+                else:
+                    msg = line.strip()+' - Unknown'
+                    with open("log_unknown.txt", "a") as log:
+                        log.write(msg+"\n")
+
+                print msg
+
+            except KeyboardInterrupt:
+                print 'Quit by keyboard interrupt sequence!'
+                break
+            except HttpResponseError:
+                msg = line.strip()+' - HttpResponseError'
                 print msg
                 with open("log_Error.txt", "a") as log:
                     log.write(msg+"\n")
-            else:
-                pass
+            except:
+                if line:
+                    msg = url.strip()+' - CatchAllError'
+                    print msg
+                    with open("log_Error.txt", "a") as log:
+                        log.write(msg+"\n")
+                else:
+                    pass
 
 if __name__ == "__main__":
     try:
